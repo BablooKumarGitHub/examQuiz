18e60f7fe9f8fa689194aa30179b21c200312342
bablookumarbhardvaj@gmail.com <97409491+BablooKumarGitHub@users.noreply.github.com>
2023-05-26 15:25:39 +0530

    c

---
+++ examQuiz/README.md
@@ -0,0 +1,2 @@
+# examQuiz
+  This is Exam Quizzes Web Application based on Spring Boot , MySQL, Angular JS, Spring Security and etc.
---
+++ examQuiz/src/test/java/com/examQuiz/examQuiz/ExamQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examQuiz.examQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
